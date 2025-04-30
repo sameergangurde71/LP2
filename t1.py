2bfd7ffcbad6ce42b85551d88893e28f9940f18f
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2025-04-30 10:18:07 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,2 +1,11 @@
 print("hello world")
 print("te")
+
+def selectionSort():
+    for i in range (len(arr)):
+        min = float('inf')
+        for j in range(i+1,len(arr)):
+          if(arr[i]>arr[j]):
+            arr[i],arr[j] = arr[j],arr[i]
+
+print(selctionSort([89,75,41,23,65,12,36,10])
